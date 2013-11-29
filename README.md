901c78bf6d643b85d8d550a3d02e85f8c1a21f1e
knightli <seven3721.inf@gmail.com>
2013-11-29 14:03:50 +0800

    first commit

--- README.md
+++ README.md
@@ -1,4 +1,165 @@
-IMReport
-========
+IMReport 可定制的上报组件
+================
 
-鍙畾鍒剁殑jsreport涓婃姤缁勪欢
+## 一 特性
+ - 使用UMD方式定义, 兼容AMD/CMD及非模块规范
+ - 调用统一的上报cgi
+ - 上报节流(合并短时间内的连续上报,减少上报请求数)
+ - 可根据项目情况使用工厂方法定制自己的上报器(可定制不同的mid和默认行为)
+ - 定制上报器支持字符串MAP(与上报需求一一对应, 简化上报点, 方便后续维护)
+
+## 二 如何引入和使用:
+
+### 1. AMD/CMD环境下
+例2.1.1
+```
+require('bower_components/IMReport/IMReport',function(IMReport){
+    //1. 进行全局配置
+    IMReport.config({
+        //...
+    });
+    
+    //2. 创建上报器实例, 定制上报器参数和map列表
+    var reportInstance = IMReport.ReportFactory({
+        config: { //一个map表, key为上报项, value为上报参数
+            'REPORT_POINT_A':{'aId':1,'mId':100}
+        }
+    });
+    
+    //3. 在业务代码中使用实例的report方法上报
+    function doSth(){
+        //bussiness code
+        reportInstance.report('REPORT_POINT_A');
+        //...
+    }
+});
+```
+
+### 2. 非模块化环境下
+例2.1.2
+html内
+```
+<!-- 引入核心js -->
+<script src="bower_components/IMReport/IMReport"></script>
+<script>
+(function(){
+    //核心js在找不到define方法时会选择暴露IMReport方法在window下
+    //所以下面的代码直接用IMReport即可
+    
+    IMReport.config({
+        //...
+    });
+    
+    var reportInstance = IMReport.ReportFactory({
+        //...
+    });
+    
+    function doSth(){
+        //bussiness code
+        reportInstance.report('XXX');
+        //...
+    }
+})();    
+</script>
+```
+
+## 三 关于上报参数的说明
+### 1. 背景
+通常, 我们会调用统一的jsreport cgi进行数据上报  
+统一的上报cgi支持传一系列参数, 可上报到不同的系统( monitor imdata ads...)  
+
+但这个cgi的上报参数是靠约定进行字符串拼接的  
+`
+actionId-value-monitorId-flag1-flag2-flag3-ver-rev
+`
+
+用起来非常麻烦!
+
+本组件就是为了前端上报方便而诞生的  
+core是amd写的, knight在core的基础上封装了一层factory以及map的逻辑.
+
+### 2. 参数说明
+下面按照js里使用时的形式列一下有哪些参数:  
+
+*前面是js里使用时的参数字符串,  
+后面是完整的字符串(可对应上面的拼接顺序看)以及说明  
+带方括号的是可选参数  
+不带方括号的参数在上报时至少要有一个*
+
+     - `mId`      monitorId  monitor系统的上报id
+     - `aId`      actionId   数分:行为id
+     - `v`        [value]    数分:行为value
+     - `flag1`    [flag1]    数分:行为的标志位1
+     - `flag2`    [flag2]    数分:行为的标志位2
+     - `flag3`    [flag3]    数分:行为的标志位3
+     - `ver`      [ver]      数分:版本
+     - `rev`      [rev]      数分:附加参数
+
+## 四 接口说明
+
+###core: config(object)   
+####配置上报的全局默认参数
+
+例4.1.1:
+```
+IMReport.config({//
+	'bId': 192, //qqm分配的jsreport的msgQ ID
+	'aId': function (mId){ return '0';}, //@amd补充下这个参数的注释?
+	'ctype': 2 //上报cgi类型，默认是需要登录态，type=2，是不需要登录态
+});
+```
+
+###core: ReportFactory(object)
+####创建一个定制的上报器
+例4.2.1:
+```
+var isVisitor = $.bom.getHash('visitor') ? 1:0;
+var from = $.bom.getHash('from');
+
+var reportInstance = IMReport.ReportFactory({
+    config: {
+        'PAGE_ENTER': 800001, //注:仅传一个数字时该数字表示mId
+        'DOWNLOAD_BTN_CLICK':{'aId':9002,'mId':800002},
+        'DOWNLOAD_BTN_CLICK#SUCC':{'aId':9003,'mId':800003}
+    },
+    getDefaultOptFn:function(usrParam){
+        //定制上报器特有的上报行为(如果不传此函数, 意味着本上报器没有定制的特殊上报行为)
+        var opt = {};
+        
+        opt.bId = 193; //定制上报器默认bId,该bId优先级高于全局的bId
+        
+        opt.flag1 = isVisitor; //如果产品要求区分一些上报情景, 可能会要求aId上报时带上flag,用约定的不同的值来做统计用
+        
+        //统一在aId上报时, 对v进行处理(from create: 1, else: 0)
+        if( (typeof usrParam==='object')
+            && ('aId' in usrParam)
+            && !('v' in usrParam)){
+            opt.v = (from && from==='create') ? 1 : 0;
+        }
+    }
+});
+```
+
+###instance: report(string[, object])
+####在具体上报点调用的上报方法
+注意: 必须先调用上面的`IMReport.ReportFactory()`来得到一个上报器实例, 然后再调用该方法
+
+例4.3.1:(最简单也是最常用的上报调用)
+```
+...
+reportInstance.report('PAGE_ENTER');
+...
+```
+
+例4.3.1:(指定上报点参数的上报调用)
+```
+reportInstance.report('DOWNLOAD_BTN_CLICK',{
+    bId: 194, //上报点指定的bId, 优先级最高
+    v: getRoleType=='owner' ? 1:0 //上报点指定的v, 优先级最高
+});
+```
+
+
+## TODO
+ - 和grunt task整合, 做到自动维护上报MAP表
+ - 完善测试用例和demo
---
+++ src/IMReport.js
@@ -0,0 +1,212 @@
+﻿/**
+ * @description IMReport report封装 
+ * @author amadeusguo/knightli
+ * readme: XXXX
+ * sample: XXXX
+ */
+ 
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define([], factory);
+    } else {
+        // Browser globals
+        root['IMReport'] = factory();
+    }
+}(this, function () {
+    
+    //=======  AMD module inner code ========
+    
+    //==== handbar functions begin  ==== (或许来自其他库,但为了消除依赖,这里适度重复了)  TODO:这里要考虑重复代码的同步更新机制!!
+    function indexOf(array,elem) {
+        //IE不支持这个方法
+        if(Array.prototype.indexOf){
+            return Array.prototype.indexOf.call( array, elem );
+        }
+        for ( var i = 0, length = array.length; i < length; i++ ) {
+            if ( array[ i ] === elem ) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+    //==== handbar functions end  ====
+    
+    
+    /*
+     * 使用jsreport cgi上报monitor和数分，可以合并上报多个
+     * 
+     * A) 配置上报, bId: 业务Id，找qqm领
+     *              aId: 函数通过mID算出数分的上报id，不配也可以
+                    ctype: 上报cgi类型，默认是需要登录态，type=2，目前是不需要登录态
+     * $.report.config({
+     *     'bId': 150,
+     *     'aId': function (mId){
+     *         // 这个100个上报行为ID给数分
+     *         if (mId>=279685 && mId<279785)
+     *             return mId - 279684;
+     *         else
+     *             return 0;
+     *     },
+        'ctype' : 2
+     * });
+     * 
+     * B) 上报示例
+     *
+     * 1. 上报1次到monitor:  $.report(mId)
+     * 2. 只上报一次，例如UV: $.report(mId, 'once')
+     * 3. 不延迟合，立即上报: $.report(mId, 'now')
+     * 4. 上报两次: $.report(mId, 2)
+     * 5. $.report(mId, 2, 'once')
+     * 6. $.report(mId, 2, 'now')
+     * 7. 上报monitorId, 群号, 客户端版本号: $.report({mId:123, flag1:gc, ver:5093})
+     * 8. 上报更多的内容: $.report({mId:1203, aId: 12, v:1, flag1:333, flag2:333, flag3:333, ver:5093, rev:})
+     * 
+     * cgi上报顺序
+     * actionId-value-monitorId-flag1-flag2-flag3-ver-rev
+     */
+    
+    
+    //==== module code begin ====
+    var _bId, _url;
+    var _aId = function (){
+        return 0;
+    };
+    
+    var config = function (conf){
+        
+        _bId = conf['bId'];
+        _url = 'http://jsreport.qq.com/cgi-bin/report'+(conf['ctype']?conf['ctype']:"")+'?id='+_bId+'&rs=';
+
+        if (conf['aId']){
+            _aId = conf['aId'];
+        }
+    };
+
+    var timer, // 用于延迟上报的timer
+        cache = [],
+        done = []; // 只上报一次的队列
+
+    var doReport = function () {
+        var img = new Image();
+        img.src = _url + cache.join('|_|') + '&r=' + Math.random();
+        img = null;
+
+        // clear report cache
+        cache = [];
+    };
+
+    var keys = ['aId', 'v', 'mId', 'flag1', 'flag2', 'flag3', 'ver', 'rev'];
+    var report = function (mId, value, type) {
+        // 参数适配
+        var args = {};
+        var msg;
+
+        var t = typeof mId;
+        if (t=='number'){
+
+            t = typeof value;
+            if ( t=='undefined' || t=='string' ){
+                args.v = 1;
+                type = value;
+
+                msg = _aId(mId)+'-1-'+mId;
+
+            }else if (t == 'number'){
+                args.v = value;
+
+                msg = _aId(mId)+'-1-'+mId;
+            }
+
+        }else if (t=='object'){
+            args = mId;
+            mId = args.mId;
+            type = value;
+
+            if (!('v' in args)) args.v = 1;
+            if (!('aId' in args)) args.aId = _aId(mId);
+
+            var msg_arr = [];
+            for (var key in args){
+                var idx = indexOf(keys, key);
+                if (idx != -1){
+                    msg_arr[idx] = args[key];
+                }
+            }
+            msg = msg_arr.join('-').replace(/-(?=-)/g, '-0');
+        }
+
+        if (type == 'once') {
+            // once 只上报一次
+            if (indexOf(done, mId) != -1) return;
+            done.push(mId);
+        }
+
+        if (msg) cache.push(msg);
+
+        clearTimeout(timer);
+        if (type == 'now' || mId == 'now') {
+            // now立马上报
+            doReport();
+        } else {
+            timer = setTimeout(doReport, 500);
+        }
+    };
+
+    report.ReportFactory = (function(){
+
+        var keys = {'aId':1, 'v':1, 'mId':1, 'flag1':1, 'flag2':1, 'flag3':1, 'ver':1, 'rev':1};
+
+        var commonReport = function(param, fn){
+            if(!param) return;
+
+            var p = {};
+            if(typeof fn==='function') p = fn(param);
+
+
+            if(typeof param === 'number'){
+                p.mId = param;
+            }
+            else if(typeof param === 'object'){
+                for (var key in param){
+                    if(key in keys){
+                        p[key] = param[key];
+                    }
+                }
+            }
+
+            report(p);
+        };
+
+        var factory = function(opt){
+            opt = opt || {};
+            var _config = opt.config || {};
+            var _fn = opt.getDefaultOptFn;
+
+            return function(key,param){
+                var p = _config[key];
+                if(!p) return;
+
+                if(typeof param==='object'){
+                    for(var i in param){
+                        p[i] = param[i];
+                    }
+                }
+
+                commonReport(p,_fn);
+            };
+        };
+
+        return factory;
+    })();
+
+    report.config = config;
+    
+    //==== module code end ====
+    
+    return report;
+    
+    //=======  AMD module inner code ========
+
+}));
